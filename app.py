710ba3d057bb0126ba0d670034ac21b9cd5a9dad
Yaakov Shitrit <71325501+yaakovsh8@users.noreply.github.com>
2025-01-21 21:03:59 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+# דף השאלות והתשובות כעמוד הראשי
+@app.route('/')
+def questions():
+    return render_template("questions.html")
+
+# דף עם הקישורים לשאלות ותשובות ולגיט
+@app.route('/home')
+def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
