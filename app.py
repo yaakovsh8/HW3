29bf334b72e8f1cd0b39038c5c3407f8ca5a19f2
Yaakov Shitrit <71325501+yaakovsh8@users.noreply.github.com>
2025-01-21 22:12:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# נתיב לדף הראשי - שאלות ותשובות
-@app.route('/')
-def questions():
-    return render_template("questions.html")
-
-# נתיב לדף index הראשי (אם תרצה לשמור אותו)
-@app.route('/home')
+# Route for the home page (index.html)
+@app.route("/")
 def home():
     return render_template("index.html")
+
+# Route for the questions page (questions.html)
+@app.route("/questions")
+def questions():
+    return render_template("questions.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
